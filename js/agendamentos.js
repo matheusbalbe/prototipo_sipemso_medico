c98a09eb30d044ce23696551e58610fcf9c9a86d
Matheus Balbe <101679909+matheusbalbe@users.noreply.github.com>
2025-08-18 10:24:24 -0300

    first commit - copia do prototipo 1

---
+++ js/agendamentos.js
@@ -0,0 +1,8 @@
+
+  function vincularMedico(){
+    document.getElementById("modalVincular").classList.add("is-open");
+  }
+  function fecharModalVinculo(){
+    document.getElementById("modalVincular").classList.remove("is-open");
+  }
+
